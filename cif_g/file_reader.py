66a996e9cdb5948905e3091f25f6b00562930a70
Daniel Kratzert <dkratzert@gmx.de>
2019-06-14 08:40:41 +0200

    Initial commit

---
+++ cif_g/file_reader.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from gemmi import cif
+
+
+def open_cif_file(filename: Path):
+    """
+    Raads a CIF file into gemmi and returns a sole block.
+    """
+    doc = cif.read_file(filename.absolute())
+    return doc.sole_block()
