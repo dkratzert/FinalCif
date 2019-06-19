2b04c230c7c6e71427906eee88d665ce10c9d266
Daniel Kratzert <dkratzert@gmx.de>
2019-06-19 23:03:38 +0200

    improved data sources

--- datafiles/datatools.py
+++ datafiles/datatools.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
+from datafiles.sadabs import Sadabs
+from datafiles.saint import SaintListFile
+
 
 class MissingCifData():
     def __init__(self):
@@ -5,3 +10,28 @@ class MissingCifData():
 
     def __setitem__(self, key, value):
         self.data[key] = value
+
+
+def get_saint():
+    """
+    returns a saint parser object from the ._ls file.
+    """
+    p = Path('./')
+    saintfiles = p.rglob('*_0m._ls')
+    saint = None
+    for s in saintfiles:
+        saint = SaintListFile(s.as_posix())
+        if saint:
+            return saint
+    return saint
+
+
+def get_sadabs():
+    p = Path('./')
+    sadfiles = p.rglob('*_0m._ls')
+    sadabs = None
+    for s in sadfiles:
+        sadabs = Sadabs(s.as_posix())
+        if sadabs:
+            return sadabs
+    return sadabs
