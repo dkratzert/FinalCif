541396f38b42b072ee3b2dcc1da0102e0fb3b975
Daniel Kratzert <dkratzert@gmx.de>
2019-06-21 08:15:46 +0200

    improved settings

--- tools/settings.py
+++ tools/settings.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QPoint, QSettings
+from PyQt5.QtCore import QPoint, QSettings, QSize
 from PyQt5.QtWidgets import QMainWindow
 
 
@@ -8,13 +8,24 @@ class FinalCifSettings():
         self.software_name = 'FinalCif'
         self.organization = 'DK'
         self.settings = QSettings(self.organization, self.software_name)
+        print(self.settings.fileName())
 
-    def save_window_position(self, position: QPoint):
-        self.settings.setValue("FinalCifWindow/position", position)
+    def save_window_position(self, position: QPoint, size: QSize):
+        self.settings.beginGroup("MainWindow")
+        self.settings.setValue("position", position)
+        self.settings.setValue("size", size)
+        self.settings.endGroup()
 
     def load_window_position(self):
-        pos = self.settings.value("FinalCifWindow/position", type=QPoint)
+        self.settings.beginGroup("MainWindow")
+        pos = self.settings.value("position", type=QPoint)
         self.window.move(pos)
+        try:
+            size = QSize(self.settings.value("size"))
+            self.window.resize(size)
+        except TypeError:
+            pass
+        self.settings.endGroup()
 
     def save_template(self, name: str, items: list):
         """
@@ -22,9 +33,12 @@ class FinalCifSettings():
         :param name: is the name of the template.
         :param items: List of key value pairs
         """
-        for item in items:
+        self.settings.beginWriteArray(name)
+        for num, item in enumerate(items):
             key, value = item
-            self.settings.setValue("Equipment/{}".format(key), value)
+            self.settings.setArrayIndex(num)
+            self.settings.setValue(key, value)
+        self.settings.endArray()
 
     def load_template(self, name):
-
+        pass
