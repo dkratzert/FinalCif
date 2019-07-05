0d62befdc5e181cf5ea1a799a10ab1a8dbb4dc4b
Daniel Kratzert <dkratzert@gmx.de>
2019-07-05 13:14:52 +0200

    license

--- scripts/make_win_realease.py
+++ scripts/make_win_realease.py
@@ -1,3 +1,11 @@
+#  ----------------------------------------------------------------------------
+#  "THE BEER-WARE LICENSE" (Revision 42):
+#  daniel.kratzert@ac.uni-freiburg.de> wrote this file.  As long as you retain
+#  this notice you can do whatever you want with this stuff. If we meet some day,
+#  and you think this stuff is worth it, you can buy me a beer in return.
+#  Dr. Daniel Kratzert
+#  ----------------------------------------------------------------------------
+
 # .\venv\Scripts\pyinstaller.exe scripts\Finalcif.spec -D     # one dir
 
 # .\venv\Scripts\pyinstaller.exe scripts\Finalcif.spec -F     # one file
