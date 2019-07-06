fae2cb85344089c81217b1bde4d9344a4071d286
Daniel Kratzert <dkratzert@gmx.de>
2019-07-06 17:48:56 +0200

    trying some things

--- gui/finalcif_gui.py
+++ gui/finalcif_gui.py
@@ -175,8 +175,6 @@ class Ui_FinalCifWindow(object):
         self.CifItemsTable.verticalHeader().setMinimumSectionSize(19)
         self.CifTableGridLayout.addWidget(self.CifItemsTable, 0, 0, 1, 3)
         self.SaveCifButton = QtWidgets.QPushButton(self.CifDataItemsGroupBox)
-        icon = QtGui.QIcon.fromTheme("text-x-generic")
-        self.SaveCifButton.setIcon(icon)
         self.SaveCifButton.setObjectName("SaveCifButton")
         self.CifTableGridLayout.addWidget(self.SaveCifButton, 2, 0, 1, 1)
         spacerItem1 = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
