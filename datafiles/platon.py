10b90bad5d941bbbcfe4e309a0acfaed7ae1eca0
Daniel Kratzert <dkratzert@gmx.de>
2019-07-06 17:48:56 +0200

    trying some things

--- datafiles/platon.py
+++ datafiles/platon.py
@@ -14,9 +14,71 @@ import subprocess
 from pathlib import Path
 from time import sleep
 
-from PyQt5.QtWidgets import QMessageBox
-
 from tools.pupdate import get_platon
+
+
+def run_platon(file: Path, param='-u', timeout=200, local_exe=True):
+    plat = None
+    chkfile = Path(file.absolute().stem + '.chk')
+    os.chdir(file.absolute().parent.parent)
+    timeticks = 0
+    # a fresh platon exe from the web:
+    if not local_exe:
+        pexe = get_platon()
+    else:
+        pexe = False
+    try:
+        si = subprocess.STARTUPINFO()
+        si.dwFlags = 1
+        si.wShowWindow = 0
+    except AttributeError:
+        si = None
+    try:
+        if pexe:
+            plat = subprocess.Popen([pexe, param, file.name], startupinfo=si)
+        else:
+            print('trying local platon')
+            print(file.name)
+            plat = subprocess.Popen(['platon', '-u', file.name], startupinfo=si)
+    except (FileNotFoundError, PermissionError) as e:
+        print(e)
+        print('Platon not found.')
+        return
+    # waiting for chk file to appear:
+    while not chkfile.is_file():
+        timeticks = timeticks + 1
+        sleep(0.01)
+        if timeticks > 1000:
+            print('Platon statup took too long. Killing Platon...')
+            try:
+                print('Platon took too much time, terminating platon. (1)')
+                plat.terminate()
+                break
+            except Exception as e:
+                print(e)
+                break
+    size1 = chkfile.stat().st_size
+    size2 = 99999999
+    timeticks = 0
+    # waiting until size does not increase anymore:
+    while size1 <= size2:
+        timeticks = timeticks + 1
+        size2 = chkfile.stat().st_size
+        # print(size1, size2)
+        sleep(0.1)
+        size1 = chkfile.stat().st_size
+        if timeticks > timeout:  # 20s
+            try:
+                print('Platon took too much time, terminating platon. (2)')
+                plat.terminate()
+                break
+            except Exception:
+                pass
+                break
+    try:
+        plat.terminate()
+    except Exception:
+        pass
 
 
 class PlatonOut():
@@ -26,7 +88,7 @@ class PlatonOut():
         self._fileobj = Path('.')
         self.filename = Path('')
         if not platfiles:
-            self.run_platon()
+            run_platon(self.cif_fileobj)
         platfiles = self.get_chkfiles()
         for platfile in platfiles:
             if platfile:
@@ -50,71 +112,13 @@ class PlatonOut():
             if line.startswith('# MoietyFormula'):
                 self.formula_moiety = ' '.join(line.split(' ')[2:])
 
-    def run_platon(self):
-        plat = None
-        chkfile = Path(self.cif_fileobj.stem + '.chk')
-        os.chdir(self.cif_fileobj.absolute().parent)
-        timeticks = 0
-        # a fresh platon exe from the web:
-        pexe = get_platon()
-        try:
-            si = subprocess.STARTUPINFO()
-            si.dwFlags = 1
-            si.wShowWindow = 0
-        except AttributeError:
-            si = None
-        try:
-            if pexe:
-                plat = subprocess.Popen([pexe, '-u', self.cif_fileobj.name], startupinfo=si)
-            else:
-                print('trying local platon')
-                plat = subprocess.Popen([r'platon.exe', '-u', self.cif_fileobj.name], startupinfo=si)
-        except (FileNotFoundError, PermissionError):
-            print('Platon not found.')
-            return
-        # waiting for chk file to appear:
-        while not chkfile.is_file():
-            timeticks = timeticks + 1
-            sleep(0.01)
-            if timeticks > 1000:
-                print('Platon statup took too long. Killing Platon...')
-                try:
-                    print('Platon took too much time, terminating platon. (1)')
-                    plat.terminate()
-                    break
-                except Exception as e:
-                    print(e)
-                    break
-        size1 = chkfile.stat().st_size
-        size2 = 99999999
-        timeticks = 0
-        # waiting until size does not increase anymore:
-        while size1 <= size2:
-            timeticks = timeticks + 1
-            size2 = chkfile.stat().st_size
-            #print(size1, size2)
-            sleep(0.1)
-            size1 = chkfile.stat().st_size
-            if timeticks > 200:  # 30s
-                try:
-                    print('Platon took too much time, terminating platon. (2)')
-                    plat.terminate()
-                    break
-                except Exception:
-                    pass
-                    break
-        try:
-            plat.terminate()
-        except Exception:
-            pass
-
     def __repr__(self):
         return 'Platon:\n{}'.format(self.formula_moiety)
 
 
 if __name__ == '__main__':
     print('###############\n\n')
-    fname = Path(r'D:\frames\guest\Esser_JW283\Esser_JW283\Esser_JW283_0m_a.cif')
+    fname = Path(r'test-data/DK_zucker2_0m.cif')
     s = PlatonOut(fname)
     print(s)
     chkfile = Path(fname.stem + '.chk')
