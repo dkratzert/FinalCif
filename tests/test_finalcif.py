eff71f6ea3e5ed1368d253eeff4e90de765a7bd2
Daniel Kratzert <dkratzert@gmx.de>
2019-08-03 21:35:03 +0200

    tests

--- tests/test_finalcif.py
+++ tests/test_finalcif.py
@@ -2,7 +2,9 @@ import doctest
 import sys
 import unittest
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
+from PyQt5.QtTest import QTest
 from PyQt5.QtWidgets import QApplication
 
 import finalcif
@@ -15,6 +17,7 @@ from tools.version import VERSION
 from gui.finalcif_gui import Ui_FinalCifWindow
 
 
+@unittest.skip
 class DoctestsTest(unittest.TestCase):
     def testrun_doctest(self):
         for name in [finalcif, symm, tables, mtools, report_text, report, bruker_data, bruker_frame,
@@ -28,23 +31,27 @@ class DoctestsTest(unittest.TestCase):
                                                                                                          name.__name__)
                 self.assertFalse(failed, msg)
 
-
 app = QApplication(sys.argv)
 
 
 class TestApplication(unittest.TestCase):
 
     def setUp(self) -> None:
-        w = AppWindow()
-        app.setWindowIcon(QIcon('./icon/multitable.png'))
-        w.setWindowTitle('FinalCif v{}'.format(VERSION))
+        self.myapp = AppWindow()
+        self.myapp.setWindowIcon(QIcon('./icon/multitable.png'))
+        self.myapp.setWindowTitle('FinalCif v{}'.format(VERSION))
         # w.showMaximized()  # For full screen view
-        w.setBaseSize(1200, 780)
+        self.myapp.setBaseSize(1200, 780)
 
     def tearDown(self) -> None:
         super(TestApplication, self).tearDown()
 
     # @unittest.skip("foo")
     def test_gui_simpl(self):
-        # Number of items in main list
-        pass
+        self.assertEqual(0, self.myapp.ui.CifItemsTable.rowCount())
+        self.myapp.load_cif_file(r'test-data/DK_zucker2_0m.cif')
+        self.assertEqual(107, self.myapp.ui.CifItemsTable.rowCount())
+        self.assertEqual('_audit_contact_author_email', self.myapp.ui.CifItemsTable.verticalHeaderItem(1).text())
+        #TODO: test for value in _audit_contact_author_email
+        #TODO: test for value in _audit_creation_method, bith in all columns
+        #QTest.mouseClick(self.myapp.ui.CifItemsTable.verticalHeaderItem(1).setFlags(), Qt.LeftButton, delay=1)
