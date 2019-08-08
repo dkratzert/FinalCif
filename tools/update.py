256ca374fd5c797c511c8d3e2d3ca084d7ca5818
Daniel Kratzert <dkratzert@gmx.de>
2019-08-08 09:46:47 +0200

    version 16 + version check

--- tools/update.py
+++ tools/update.py
@@ -5,13 +5,15 @@
 #  and you think this stuff is worth it, you can buy me a beer in return.
 #  Dr. Daniel Kratzert
 #  ----------------------------------------------------------------------------
+import ssl
+
 import urllib3
 
-mainurl = "https://xs3-data.uni-freiburg.de/finalcif/"
+mainurl = "http://xs3-data.uni-freiburg.de/finalcif/"
 
 
 def get_current_version():
-    http = urllib3.PoolManager()
+    http = urllib3.PoolManager(strict=False, cert_reqs=ssl.CERT_NONE)
     r = http.request('GET', '{}version.txt'.format(mainurl))
     if r.status == 200:
         return int(r.data.decode('ascii'))
