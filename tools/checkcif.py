c9c2779fea7ba0759c30f25cf5c1160e5f418ed8
Daniel Kratzert <dkratzert@gmx.de>
2019-08-08 14:07:37 +0200

    online checkcif works

--- tools/checkcif.py
+++ tools/checkcif.py
@@ -5,22 +5,21 @@
 #  and you think this stuff is worth it, you can buy me a beer in return.
 #  Dr. Daniel Kratzert
 #  ----------------------------------------------------------------------------
-import sys
 from pathlib import Path
 
 import requests
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl, QPoint
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
 
 
 class WebPage(QWebEngineView):
-    def __init__(self, url):
+    def __init__(self, file: Path):
         QWebEngineView.__init__(self)
         # self.load(QUrl("https://checkcif.iucr.org/"))
-        url = QUrl.fromLocalFile(url)
-        self.load(url)
-        #self.loadFinished.connect(self._on_load_finished)
+        self.url = QUrl.fromLocalFile(str(file.absolute()))
+        self.load(self.url)
+        # self.loadFinished.connect(self._on_load_finished)
 
     def _on_load_finished(self):
         self.page().toHtml(self.Callable)
@@ -30,38 +29,67 @@ class WebPage(QWebEngineView):
         self.html = html_str
 
 
-def get_checkcif():
-    file_name = 'test-data/DK_zucker2_0m.cif'
-    out_file = "cifreport.html"
-    f = open(file_name, 'rb')
+class MakeCheckCif():
 
-    headers = {
-        "file"      : f,  # Path(file_name).open(),  # .read_text(encoding='ascii'),
-        "runtype"   : "symmonly",
-        "referer"   : "checkcif_server",
-        "outputtype": 'HTML',
-        "validtype" : "checkcif_only",
-        # "valout"    : 'vrfno',
-        # "UPLOAD"    : 'submit',
-    }
-    print('Report request sent')
-    url1 = url = 'http://checkcif.iucr.org'
-    url = 'https://checkcif.iucr.org/cgi-bin/checkcif_hkl.pl'
+    def __init__(self, parent, cif: Path, outfile: Path):
+        self.parent = parent
+        # _, self.out_file = mkstemp(suffix='.html')
+        self.out_file = outfile
+        self.show_report_window(cif)
 
-    # r1 = requests.get(url1)
-    # for i in r1.cookies:
-    #    print(i, '##')
+    def get_checkcif(self, file_name: str, out_file: Path):
+        """
+        Requests a checkcif run from IUCr servers.
+        """
+        f = open(file_name, 'rb')
 
-    r = requests.post(url, files=headers, timeout=150)
-    #print(r.text)
-    # TODO: need a tempfile here:
-    Path(out_file).write_bytes(r.content)
-    print('ready')
+        headers = {
+            "file"      : f,  # Path(file_name).open(),  # .read_text(encoding='ascii'),
+            "runtype"   : "symmonly",
+            "referer"   : "checkcif_server",
+            "outputtype": 'HTML',
+            "validtype" : "checkcif_only",
+            # "valout"    : 'vrfno',
+            # "UPLOAD"    : 'submit',
+        }
+        print('Report request sent')
+        url = 'https://checkcif.iucr.org/cgi-bin/checkcif_hkl.pl'
+        r = requests.post(url, files=headers, timeout=150)
+        out_file.write_bytes(r.content)
+        f.close()
+        print('ready')
+        return out_file
+
+    def show_report_window(self, cif):
+        self.get_checkcif(cif.absolute(), self.out_file)
+        app = QMainWindow(self.parent)
+        web = WebPage(self.out_file)
+        app.setCentralWidget(web)
+        app.setBaseSize(900, 900)
+        app.show()
+        app.setMinimumWidth(900)
+        app.setMinimumHeight(700)
+        app.move(QPoint(100, 50))
+        web.show()
 
 
 if __name__ == "__main__":
-    get_checkcif()
-    app = QApplication(sys.argv)
-    web = WebPage('/Users/daniel/GitHub/FinalCif/cifreport.html')
-    web.show()
-    sys.exit(app.exec_())
+    # outfile = get_checkcif('test-data/p21c.cif')
+    # app = QApplication(sys.argv)
+    # web = WebPage(outfile)
+    # web.show()
+    # app.exec_()
+    # web.close()
+    # try:
+    #    Path(outfile).unlink()
+    # except PermissionError:
+    #    print('can not delete 1')
+
+    #outfile = get_checkcif('test-data/p21c.cif')
+    #app = QWindow()
+    #web = WebPage(outfile)
+    #web.show()
+    #app.setWidth(500)
+    #app.exec_()
+    #web.close()
+    pass
