33a24235a0db1678171736acdd18147b22f820f9
Daniel Kratzert <dkratzert@gmx.de>
2019-08-08 22:09:36 +0200

    fix for author template

--- finalcif.py
+++ finalcif.py
@@ -27,11 +27,11 @@ if getattr(sys, 'frozen', False):
     # If the application is run as a bundle, the pyInstaller bootloader
     # extends the sys module by a flag frozen=True and sets the app
     # path into variable _MEIPASS'.
+    os.environ['PATH'] = sys._MEIPASS + os.pathsep + os.environ['PATH']
     application_path = sys._MEIPASS
 else:
     application_path = os.path.dirname(os.path.abspath(__file__))
 
-os.environ['PATH'] = sys._MEIPASS + os.pathsep + os.environ['PATH']
 
 if DEBUG:
     from PyQt5 import uic
