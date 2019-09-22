cc22a7e0abbbda6e868ddf7d8f25ebaef7b34892
Daniel Kratzert <dkratzert@gmx.de>
2019-09-22 15:28:48 +0200

    version 30

--- finalcif.py
+++ finalcif.py
@@ -26,7 +26,7 @@ from tools.checkcif import MakeCheckCif, MyHTMLParser
 from tools.update import mainurl
 from tools.version import VERSION
 
-DEBUG = True
+DEBUG = False
 
 if getattr(sys, 'frozen', False):
     # If the application is run as a bundle, the pyInstaller bootloader
