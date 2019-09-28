df496d4866e76b7045eb2f49035e7a8c2823b20f
Daniel Kratzert <dkratzert@gmx.de>
2019-09-28 21:09:14 +0200

    nearly finished vref editor

--- gui/vrf_classes.py
+++ gui/vrf_classes.py
@@ -1,11 +1,58 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QHBoxLayout, QLabel, QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout, QWidget, QFrame, \
-    QLineEdit, QTextEdit
+import textwrap
+
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QDialog, QFrame, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QSpacerItem, QTextEdit, \
+    QVBoxLayout, QWidget
+
+from gui.custom_classes import QHLine
+
+
+class VREF():
+    """
+    _vrf_PLAT699_DK_zucker2_0m
+    ;
+    PROBLEM: Missing _exptl_crystal_description Value .......     Please Do !
+    RESPONSE: ...
+    ;
+    """
+
+    def __init__(self):
+        self.name = ''
+        self._problem = 'PROBLEM: '
+        self._response = 'RESPONSE: '
+
+    @property
+    def response(self):
+        return self._response
+
+    @response.setter
+    def response(self, txt):
+        self._response = self.response + '\n'.join(textwrap.wrap(txt, width=70))
+
+    @property
+    def problem(self):
+        return self._problem
+
+    @problem.setter
+    def problem(self, txt):
+        self._problem = self.problem + txt
+
+    def __repr__(self):
+        txt = ("{}\n"
+               ";\n"
+               "{}\n"
+               "{}\n"
+               ";\n".format(self.name, self.problem, self.response)
+               )
+        return txt
+
+    def __str__(self):
+        return self.__repr__()
 
 
 class MyVRFContainer(QWidget):
 
-    def __init__(self, form: dict, parent=None):
+    def __init__(self, form: dict, help: str, parent=None):
         """
         A Widget to display each validation response form.
         #TODO: test what happens if form is empty or contains garbage
@@ -19,40 +66,71 @@ class MyVRFContainer(QWidget):
         """
         super().__init__(parent)
         self.form = form
-        #self.setMinimumWidth(400)
-        self.color = 'red'  # TODO: get colr from alert type
+        # self.setMinimumWidth(400)
         self.mainVLayout = QVBoxLayout(self)
         self.setLayout(self.mainVLayout)
-        #self.setStyleSheet('QWidget { border: 2px solid black }')
+        # self.setStyleSheet('QWidget { border: 2px solid black }')
         self.mainVLayout.setContentsMargins(0, 0, 0, 0)
         self.mainVLayout.setSpacing(0)
+        self.mainVLayout.addWidget(QHLine())
         # The button to get help for the respective alert:
         self.helpbutton = QPushButton('Help')
+        self.helpbutton.clicked.connect(self.show_help)
+        self.response_text_edit = QTextEdit()
         self.alert_label_box()
         self.problem_label_box()
         self.response_label_box()
+        self.help = help
         #
         self.show()
+
+    def show_help(self):
+        dialog = QDialog(self)
+        layout = QVBoxLayout()
+        label = QLabel(self.help)
+        layout.addWidget(label)
+        label.adjustSize()
+        dialog.setLayout(layout)
+        dialog.adjustSize()
+        # dialog.setFixedHeight(200)
+        # dialog.setFixedWidth(200)
+        dialog.show()
+        dialog.raise_()
+
+    def sizeHint(self) -> QSize:
+        return QSize(400, 150)
 
     def alert_label_box(self):
         frame = QFrame()
         hlayout = QHBoxLayout()
-        hlayout.setContentsMargins(4, 4, 4, 4)
+        hlayout.setContentsMargins(4, 8, 4, 4)
         frame.setLayout(hlayout)
         # does not work:
-        #frame.setStyleSheet('QFrame { padding: 0px; margin: 0px;}')
+        # frame.setStyleSheet('QFrame { padding: 0px; margin: 0px;}')
         label = QLabel()
         hlayout.addWidget(label)
         level = self.form['level']
         type = level[-1] if len(level) > 1 else 'General Alert'
+        color = 'lightgray'
+        if type == 'A':
+            color = 'red'
+        elif type == 'B':
+            color = 'rgb(252, 86, 3)'
+        elif type == 'C':
+            color = 'yellow'
+        elif type == 'G':
+            color = 'green'
+        if len(type) == 1:
+            type = type + ' alert'
         num = self.form['alert_num'] if 'alert_num' in self.form else ''
-        label.setText( "{} alert {}".format(type, num))
+        label.setText("{} {}".format(type, num))
         style = 'QLabel {{ font-size: 12px; background-color: {:s}; ' \
-                          'border: 2px solid black;' \
-                          'border-radius: 5px; ' \
-                          'margin: 0px;' \
-                          'padding: 4px;' \
-                '}}'.format(self.color)
+                'font-weight: bold;' \
+                'border: 2px solid black;' \
+                'border-radius: 5px; ' \
+                'margin: 0px;' \
+                'padding: 4px;' \
+                '}}'.format(color)
         label.setStyleSheet(style)
         spacerItem = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
         hlayout.addItem(spacerItem)
@@ -67,37 +145,44 @@ class MyVRFContainer(QWidget):
         p_label = QLabel()
         hlayout.addWidget(p_label, 0, Qt.AlignTop)
         p_label.setText('Problem:   ')
-        #p_label.setAlignment()
+        p_label.setAlignment(Qt.AlignLeft)
         p_label.setStyleSheet('QLabel { font-size: 12px; font-weight: bold; }')
         p_text_label = QLabel()
+        p_text_label.setAlignment(Qt.AlignLeft)
         hlayout.addWidget(p_text_label)
+        hlayout.setAlignment(Qt.AlignLeft)
         p_text_label.setText(self.form['problem'])
         self.mainVLayout.addWidget(frame)
 
     def response_label_box(self):
         frame = QFrame()
         hlayout = QHBoxLayout()
-        hlayout.setContentsMargins(4, 4, 4, 4)
+        hlayout.setContentsMargins(4, 8, 4, 4)
         frame.setLayout(hlayout)
         resp_label = QLabel()
         hlayout.addWidget(resp_label, 0, Qt.AlignTop)
         resp_label.setText('Response: ')
         resp_label.setStyleSheet('QLabel { font-size: 12px; font-weight: bold }')
-        response_text_edit = QTextEdit()
-        hlayout.addWidget(response_text_edit)
+        self.response_text_edit.setFocusPolicy(Qt.StrongFocus)
+        hlayout.addWidget(self.response_text_edit)
         self.mainVLayout.addWidget(frame)
 
-if __name__ == '__main__':
-    from PyQt5.QtWidgets import QApplication
-    import sys
 
+if __name__ == '__main__':
+    """
     app = QApplication(sys.argv)
-    form = {'level'     : 'PLAT035_ALERT_1_B',
-            'name'      : '_vrf_PLAT035_DK_zucker2_0m',
-            'problem'   : '_chemical_absolute_configuration Info  Not Given     Please Do '
-                          '!  ',
+    form = {'level'    : 'PLAT035_ALERT_1_B',
+            'name'     : '_vrf_PLAT035_DK_zucker2_0m',
+            'problem'  : '_chemical_absolute_configuration Info  Not Given     Please Do '
+                         '!  ',
             'alert_num': 'PLAT035'}
-    web = MyVRFContainer(form)
+    web = MyVRFContainer(form, parent=None)
     app.exec_()
     web.raise_()
+    """
+    v = VREF()
+    v.name = '_vrf_PLAT035_DK_zucker2_0m'
+    v.problem = '_chemical_absolute_configuration Info  Not Given     Please Do '
+    v.response = 'Yes, this is a very long response and I want to see if this is wrapping correctly around ma 70 zeichen limit which is critical for cif. I also have to quote the special characters!'
 
+    print(v)
