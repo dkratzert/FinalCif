d8bdcae0f4e2d64631182613cab7e040c25ef602
Daniel Kratzert <dkratzert@gmx.de>
2020-01-17 13:30:37 +0100

    Update make_win_release.bat

--- scripts/make_win_release.bat
+++ scripts/make_win_release.bat
@@ -1,8 +1,10 @@
 
-cd ..
 
-.\venv\Scripts\activate
 
-pip install -r ..\requirements.txt
+rem .\venv\Scripts\activate
 
-.\venv\Scripts\pyinstaller.exe -D Finalcif_installer.spec --clean -y
+pip install -r requirements.txt
+
+pyinstaller.exe -D Finalcif_installer.spec --clean -y
+
+"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" scripts\finalcif-install_win64.iss
