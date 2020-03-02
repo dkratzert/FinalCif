960497fc822eab6786168d8eccc1e766c234d414
Daniel Kratzert <dkratzert@gmx.de>
2020-03-02 20:18:18 +0100

    improved string translation

--- cif/cif_file_io.py
+++ cif/cif_file_io.py
@@ -37,6 +37,13 @@ def quote(string: str, wrapping=80) -> str:
 
 charcters = {'°'      : r'\%',
              '±'      : r'+-',
+             '×'      : r'\\times',
+             '≠'      : r'\\neq',
+             '→'      : '\\rightarrow',
+             '←'      : '\\leftarrow',
+             '∞'      : '\\infty',
+             '≈'      : '\\sim',
+             '≃'      : '\\simeq',
              'ß'      : r'\&s',
              'ü'      : r'u\"',
              'ö'      : r'o\"',
@@ -45,15 +52,38 @@ charcters = {'°'      : r'\%',
              'è'      : r'\`e',
              'ó'      : r"\'o",
              'ò'      : r'\`o',
-             u'\u022F': r'\.o',
+             'ȯ'      : r'\.o',
+             'ø'      : r'\/o',
              'á'      : r'\'a',
              'à'      : r'\`a',
              'â'      : r'\^a',
+             'å'      : r'\%a',
              'ê'      : r'\^e',
              'î'      : r'\^i',
              'ô'      : r'\^o',
              'û'      : r'\^u',
              'ç'      : r'\,c',
+             'ñ'      : r'\~n',
+             'ł'      : r'\/l',
+             'đ'      : r'\/d',
+             u'\u2079': r'^9^',
+             u'\u2078': r'^8^',
+             u'\u2077': r'^7^',
+             u'\u2076': r'^6^',
+             u'\u2075': r'^5^',
+             u'\u2074': r'^4^',
+             u'\u00B3': r'^3^',
+             u'\u00B2': r'^2^',
+             u'\u2081': r'^1^',
+             u'\u2082': r'^2^',
+             u'\u2083': r'^3^',
+             u'\u2084': r'^4^',
+             u'\u2085': r'^5^',
+             u'\u2086': r'^6^',
+             u'\u2087': r'^7^',
+             u'\u2088': r'^8^',
+             u'\u2089': r'^9^',
+             u'\u0131': r'\?i',
              u"\u03B1": r'\a',  # alpha
              u"\u03B2": r'\b',  # beta
              u"\u03B3": r'\g',  # ...
@@ -85,9 +115,7 @@ def set_pair_delimited(block, key: str, txt: str):
     """
     Converts special characters to their markup counterparts.
     """
-    for char in txt:
-        if char in charcters:
-            txt = txt.replace(char, charcters[char])
+    txt = utf8_to_str(txt)
     try:
         # bad hack to get the numbered values correct
         float(txt)
@@ -98,6 +126,18 @@ def set_pair_delimited(block, key: str, txt: str):
             block.set_pair(key, txt)
         else:
             block.set_pair(key, quote(txt))
+
+
+def utf8_to_str(txt):
+    """
+    Translates an utf-8 text to a CIF ascii string.
+    :param txt: utf-8 text
+    :return: delimited ascii string
+    """
+    for char in txt:
+        if char in charcters:
+            txt = txt.replace(char, charcters[char])
+    return txt
 
 
 def retranslate_delimiter(txt: str) -> str:
