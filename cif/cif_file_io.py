8773338b3620a3bd71514d97a179cbca3cb9becc
Daniel Kratzert <dkratzert@gmx.de>
2020-09-15 13:44:04 +0200

    playing with loops

--- cif/cif_file_io.py
+++ cif/cif_file_io.py
@@ -166,6 +166,33 @@ class CifContainer():
         return loops
 
     @property
+    def n_loops(self):
+        return len(self.loops)
+
+    @property
+    def loops_tags(self) -> list:
+        tags = []
+        for n in range(self.n_loops):
+            tags.append(self.loops[n].tags)
+        return tags
+
+    def loops_as_list(self):
+        """
+        for t in c.loops_as_list():
+            for row in t:
+                print(row)
+
+        >>> loop = self.block.init_loop('_ocean_', ['id', 'name'])
+        >>> # empty table is invalid in CIF, we need to add something
+        >>> loop.add_row(['1', gemmi.cif.quote('Atlantic Ocean')])
+        """
+        tables = []
+        for num, l in enumerate(self.loops):
+            tags = self.loops_tags[num]
+            tables.append(self.block.find(tags))
+        return tables
+
+    @property
     def Z_value(self):
         return self.atomic_struct.cell.volume / self.atomic_struct.cell.volume_per_image()
 
@@ -227,26 +254,26 @@ class CifContainer():
                 x2, sig_x2 = get_error_from_value(name2coords[atom2][0])
                 y2, sig_y2 = get_error_from_value(name2coords[atom2][1])
                 z2, sig_z2 = get_error_from_value(name2coords[atom2][2])
-                delta1 = a*(x1 - x2)
-                delta2 = b*(y1 - y2)
-                delta3 = c*(z1 - z2)
-                sigd =  (
-                        (delta1 + delta2 * cos(gamma) + delta3 * cos(beta)) ** 2 * (
-                        delta1 ** 2 * A1 ** 2 + a ** 2 * (sig_x1 ** 2 + sig_x2 ** 2))
-                        + (delta1 * cos(gamma) + delta2 + delta3 * cos(alpha)) ** 2 * (
-                                delta2 ** 2 * A2 ** 2 + b ** 2 * (sig_y1 ** 2 + sig_y2 ** 2))
-                        + (delta1 * cos(beta) + delta2 * cos(alpha) + delta3) ** 2 * (
-                                delta3 ** 2 * A3 ** 2 + c ** 2 * (sig_z1 ** 2 + sig_z2 ** 2))
-                        + ((delta1 * delta2 * siggamma * sin(gamma)) ** 2 + 
-                           (delta1 * delta3 * sigbeta * sin(beta)) ** 2 + 
-                           (delta2 * delta3 * sigalpha * sin(alpha)) ** 2)) / dist ** 2
+                delta1 = a * (x1 - x2)
+                delta2 = b * (y1 - y2)
+                delta3 = c * (z1 - z2)
+                sigd = (
+                               (delta1 + delta2 * cos(gamma) + delta3 * cos(beta)) ** 2 * (
+                               delta1 ** 2 * A1 ** 2 + a ** 2 * (sig_x1 ** 2 + sig_x2 ** 2))
+                               + (delta1 * cos(gamma) + delta2 + delta3 * cos(alpha)) ** 2 * (
+                                       delta2 ** 2 * A2 ** 2 + b ** 2 * (sig_y1 ** 2 + sig_y2 ** 2))
+                               + (delta1 * cos(beta) + delta2 * cos(alpha) + delta3) ** 2 * (
+                                       delta3 ** 2 * A3 ** 2 + c ** 2 * (sig_z1 ** 2 + sig_z2 ** 2))
+                               + ((delta1 * delta2 * siggamma * sin(gamma)) ** 2 +
+                                  (delta1 * delta3 * sigbeta * sin(beta)) ** 2 +
+                                  (delta2 * delta3 * sigalpha * sin(alpha)) ** 2)) / dist ** 2
                 # The error is too large:
                 sigd = sqrt(sigd)
                 bb += sigd
                 if sigd > 0.0001:
                     count += 1
-                    print(atom1, atom2, dist, round(error, 5), round(sigd, 4), round(bb, 5), count)
-                    print('------ dist - sigma - calcsig - sum - num')
+                    # print(atom1, atom2, dist, round(error, 5), round(sigd, 4), round(bb, 5), count)
+                    # print('------ dist - sigma - calcsig - sum - num')
                     bonderrors.append(sigd)
         if len(bonderrors) > 2:
             return round(mean(bonderrors), 5)
