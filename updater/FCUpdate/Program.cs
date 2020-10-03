2a94747daf07151d99a3ac358610e78e3450da09
Daniel Kratzert <dkratzert@gmx.de>
2020-10-03 17:37:44 +0200

    updater works but clumsy

--- updater/FCUpdate/Program.cs
+++ updater/FCUpdate/Program.cs
@@ -1,24 +1,31 @@
 ﻿using System;
-using System.Diagnostics;
-using System.ComponentModel;
-using System.Linq;
 using System.Net;
 using System.IO;
+using System.Diagnostics;
+using CommandLine;
+
 
 namespace FCUpdate
 {
 
-    class ProcessList
+    class Updater
     {
-        public void FindRunningFinalCif(string[] args)
+        const string download_url = "https://xs3-data.uni-freiburg.de/finalcif/FinalCif-setup-x64-v{0}.exe";
+        const string setup_name = "FinalCif-setup-x64-v{0}.exe";
+
+        public class Options
+        {
+            [Option('v', "version", Required = true, HelpText = "Update FinalCif to this version.")]
+            public string Version { get; set; }
+        }
+
+        public void FindRunningFinalCif(string version)
         // Search and destroy FinalCif.
         {
-            string version = args[0];
-            Console.WriteLine(args);
             Console.WriteLine("FinalCif Updater");
             Console.WriteLine("Fetching update from server...");
 
-            Process[] processList = Process.GetProcessesByName("FinalCif");
+            System.Diagnostics.Process[] processList = Process.GetProcessesByName("FinalCif");
             if (processList.Length > 0) {
                 KillProcesses(processList);
             } else
@@ -27,9 +34,10 @@ namespace FCUpdate
             }
             string tmp = "C:\\Users\\daniel\\AppData\\Local\\Temp\\tmpDCB3.tmp";
             Console.WriteLine(Path.GetDirectoryName(tmp));
-            //string filename = DownloadUpdateFile(version);
-            //RunDownloadedFile(filename);
-            //DeleteUpdateFile(filename);
+            string filename = DownloadUpdateFile(version);
+            int exit_code = RunDownloadedFile(filename);
+            Console.WriteLine("exit code: {0}", exit_code);
+            DeleteUpdateFile(filename);
         }
 
         private void DeleteUpdateFile(string file)
@@ -37,13 +45,14 @@ namespace FCUpdate
             File.Delete(file);
         }
 
-        private void RunDownloadedFile(string file)
+        private int RunDownloadedFile(string file)
         {
-            //Process.Start(string.Format("FinalCif-setup-x64-v{0}.exe", version));
-            Process.Start(file);
+            var process = Process.Start(file);
+            process.WaitForExit();
+            return process.ExitCode;
         }
 
-        public void KillProcesses(Process[] processList) 
+        private void KillProcesses(Process[] processList)
         // Kills all FinalCif processes
         {
             int i = 1;
@@ -56,37 +65,47 @@ namespace FCUpdate
             }
         }
 
-        public string DownloadUpdateFile(string version)
+        private string DownloadUpdateFile(string version)
         {
 
-            string dlurl = string.Format("https://xs3-data.uni-freiburg.de/finalcif/FinalCif-setup-x64-v{0}.exe", version);
-            string tempdir = Path.GetTempPath();
+            string dlurl = string.Format(download_url, version);
+
             string tempfile = Path.GetTempFileName();
             //string combitemp = Path.Combine(tempdir, tempfile);
 
             //Console.WriteLine("Downloading to: {0}", Path.Combine(tempfile, string.Format("FinalCif-setup-x64-v{0}.exe", version)));
             Console.WriteLine("Downloading to: {0}", tempfile);
-            bool erg = false;
+
             using (var client = new WebClient())
             {
                 client.DownloadFile(dlurl, tempfile);
             }
-
-            File.Move(tempfile, Path.GetDirectoryName(tempfile));
-            return tempfile;
-        } 
-           
-    }
-
+            string tempdir = Path.GetDirectoryName(tempfile);
+            string exe_file = Path.Combine(tempdir, string.Format(setup_name, version));
+            File.Move(tempfile, exe_file);
+            return exe_file;
+        }
 
 
-
-    class Program
-    {
-        private static void Main(string[] args)
+        private static void Main(params string[] args)
         {
-           ProcessList p = new ProcessList();
-            p.FindRunningFinalCif(args);
+
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Version.Length > 0)
+                       {
+                           Updater u = new Updater();
+                           Console.WriteLine($"Version to update: -v {o.Version}");
+                           Console.WriteLine("Starting update");
+                           u.FindRunningFinalCif(o.Version);
+                       }
+                       else
+                       {
+                           Console.WriteLine("Unable to update without version number argument");
+                       
+                       }
+                   });
         }
     }
 }
