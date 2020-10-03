8c6490b0025c3adbb15a8bda4c98e7e091557ff0
Daniel Kratzert <dkratzert@gmx.de>
2020-10-03 12:36:45 +0200

    playing with C#

---
+++ updater/FCUpdate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace FCUpdate
+{
+
+    class ProcessList
+    {
+        public void FindRunningFinalCif()
+        {
+            Console.WriteLine("FinalCif Updater");
+            Console.WriteLine("Fetching update from server...");
+
+            Process[] processList = Process.GetProcessesByName("FinalCif");
+            foreach (var process in processList)
+            {
+                Console.WriteLine("Process: " + process);
+                Console.WriteLine("Ending process {0}", process.ToString());
+                process.Kill();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ProcessList p = new ProcessList();
+            p.FindRunningFinalCif();
+        }
+    }
+}
+
+
