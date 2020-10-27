e9735d0597e344e8765cb6435be0665468b7e89e
Daniel Kratzert <dkratzert@gmx.de>
2020-10-27 14:00:27 +0100

    Update make_win_release.py

--- scripts/make_win_release.py
+++ scripts/make_win_release.py
@@ -16,9 +16,12 @@ import subprocess
 import sys
 from datetime import datetime
 from pathlib import Path
+from pprint import pprint
 
 from PyQt5 import uic
 
+for key, value in os.environ.items():
+    print(key, value, '\n')
 from tools.misc import sha512_checksum
 from tools.version import VERSION
 
