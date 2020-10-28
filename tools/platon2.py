3c9f9e8a3e3df329365e98b37e69e1e97111d4a0
Daniel Kratzert <dkratzert@gmx.de>
2020-10-28 09:14:56 +0100

    Update platon2.py

--- tools/platon2.py
+++ tools/platon2.py
@@ -1,11 +1,8 @@
-import os
+import asyncio
 import subprocess
 import sys
 from pathlib import Path
-from subprocess import PIPE
-from time import sleep
 
-from PyQt5 import QtCore
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QApplication
 
@@ -15,16 +12,20 @@ from finalcif import AppWindow
 class CheckCifPlaton(QThread):
     progress = pyqtSignal(str)
     failed = pyqtSignal(str)
+    finished = pyqtSignal(str)
 
     def __init__(self, cif: Path):
         super().__init__()
         self.myapp = AppWindow()  # ([x for x in Path('.').rglob('1979688.cif')][0].absolute())
-        #self.myapp.setWindowIcon(QIcon('./icon/multitable.png'))
-        #self.myapp.setWindowTitle('FinalCif v{}'.format(VERSION))
+        # self.myapp.setWindowIcon(QIcon('./icon/multitable.png'))
+        # self.myapp.setWindowTitle('FinalCif v{}'.format(VERSION))
         self.myapp.hide()
         self.cif_fileobj = cif
+        self.chkfile = Path(self.cif_fileobj.with_suffix('.chk'))
+        self.outfile = Path('platon.out')
 
-    def run(self) -> None:
+    async def runplaton(self):
+        print('sdg#fdsgf')
         try:
             # This is only available on windows:
             si = subprocess.STARTUPINFO()
@@ -32,36 +33,30 @@ class CheckCifPlaton(QThread):
             si.wShowWindow = 0
         except AttributeError:
             si = None
-        print('running')
-        p = subprocess.run(['dist/platon', '-u', self.cif_fileobj.name], shell=True)
-        print(p.returncode)
-        while p.returncode is None:
-            self.sleep(10)
-            print('srghrsh')
-            f = Path('poutfile.chk')
-            t = f.read_text()
-            for line in t:
-                if line.startswith('20'):
-                    self.progress.emit('foobar')
+        subprocess.run(['platon', '-u', self.cif_fileobj.absolute()], shell=True)
+        print('danach')
+
+    def run(self) -> None:
+        pass
+        # probably I need a thread to run platon and another to observe the platon.out file
+        
+
 
 if __name__ == '__main__':
-    """def ckf_progress(txt: str):
+    def ckf_progress(txt: str):
         print('##foo## ' + txt)
+
 
     def ckf_finished():
         print('yeahyeah')
+
 
     app = QApplication(sys.argv)
     fname = Path('test-data/DK_zucker2_0m.cif')
     c = CheckCifPlaton(fname)
     c.progress.connect(ckf_progress)
     c.finished.connect(ckf_finished)
-    c.start()"""
-    process = QtCore.QProcess()
-    #process.start("dist/platon")
-    # or
-    process.startDetached("command")
-
+    c.start()
     """
     class Slave(QtCore.QProcess):
     def __init__(self, parent=None):
