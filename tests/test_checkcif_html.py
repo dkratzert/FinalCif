205a952ef4ee08e0e571581a47b43d2fe385fd70
Daniel Kratzert <dkratzert@gmx.de>
2020-11-06 11:42:31 +0100

    improved checkcif tests

--- tests/test_checkcif_html.py
+++ tests/test_checkcif_html.py
@@ -21,7 +21,7 @@ from tools.misc import strip_finalcif_of_name
 app = QApplication(sys.argv)
 
 
-class TestCheckCif(unittest.TestCase):
+class TestCheckCifHTML(unittest.TestCase):
 
     def setUp(self) -> None:
         os.chdir(Path(__file__).absolute().parent.parent)
@@ -34,26 +34,35 @@ class TestCheckCif(unittest.TestCase):
         self.myapp.cif.fileobj.unlink()
         Path('platon.out').unlink(missing_ok=True)
         Path('check.def').unlink(missing_ok=True)
+        Path('cu_BruecknerJK_153F40_0m-finalcif.chk').unlink(missing_ok=True)
+        Path('cu_BruecknerJK_153F40_0m-finalcif.gif').unlink(missing_ok=True)
+        Path('cu_BruecknerJK_153F40_0m-finalcif.ins').unlink(missing_ok=True)
+        Path('cu_BruecknerJK_153F40_0m-finalcif.lst').unlink(missing_ok=True)
+        Path('checkcif-cu_BruecknerJK_153F40_0m-finalcif.html').unlink(missing_ok=True)
+        Path('checkcif-cu_BruecknerJK_153F40_0m-finalcif.pdf').unlink(missing_ok=True)
+        Path('checkpdf-cu_BruecknerJK_153F40_0m-finalcif.html').unlink(missing_ok=True)
+
+    def equipment_click(self, field: str):
+        self.myapp.ui.EquipmentTemplatesStackedWidget.setCurrentIndex(0)
+        item = self.myapp.ui.EquipmentTemplatesListWidget.findItems(field, Qt.MatchStartsWith)[0]
+        self.myapp.ui.EquipmentTemplatesListWidget.setCurrentItem(item)
+        self.myapp.equipment.load_selected_equipment()
 
     #@unittest.skip('temporary skip')
     def test_checkcif_html(self):
         """Runs a html checkcif without hkl and compares the result with the html file."""
         self.maxDiff = 500
-        item = self.myapp.ui.EquipmentTemplatesListWidget.findItems('D8 VENTURE', Qt.MatchStartsWith)[0]
-        self.myapp.ui.EquipmentTemplatesListWidget.setCurrentItem(item)
-        self.myapp.equipment.load_selected_equipment()
-        item2 = self.myapp.ui.EquipmentTemplatesListWidget.findItems('Contact author', Qt.MatchStartsWith)[0]
-        self.myapp.ui.EquipmentTemplatesListWidget.setCurrentItem(item2)
-        self.myapp.equipment.load_selected_equipment()
+        self.equipment_click('D8 VENTURE')
+        self.equipment_click('Contact author')
         self.myapp.ui.cif_main_table.setText(key='_chemical_absolute_configuration', txt='ad', column=COL_EDIT)
-        #
         # Remember: This test is without structure factors!
         self.myapp.ui.structfactCheckBox.setChecked(True)
         QTest.mouseClick(self.myapp.ui.CheckcifHTMLOnlineButton, Qt.LeftButton, Qt.NoModifier)
         time.sleep(5)
         # this is the file on github:
-        self.html = Path('checkcif-' + strip_finalcif_of_name(self.myapp.cif.fileobj.stem) + '-test.html')
-        htmlfile = self.html.read_text().splitlines()[28:-13]
+        html_as_it_is_expected = Path('checkcif-' + strip_finalcif_of_name(self.myapp.cif.fileobj.stem) + '-test.html')
+        htmlfile = html_as_it_is_expected.read_text().splitlines()[28:-13]
         # this is the new downloadad file
         result = self.resobj.read_text().splitlines()[28:-13]
         self.assertEqual(htmlfile, result)
+        self.assertTrue(Path('cu_BruecknerJK_153F40_0m-finalcif.gif').exists())
--- tests/test_checkcif_platon.py
+++ tests/test_checkcif_platon.py
@@ -12,7 +12,7 @@ from tools.version import VERSION
 app = QApplication(sys.argv)
 
 
-class TestPlaton(unittest.TestCase):
+class TestPlatonCheckCIF(unittest.TestCase):
 
     def setUp(self) -> None:
         os.chdir(Path(__file__).absolute().parent.parent)
@@ -20,12 +20,15 @@ class TestPlaton(unittest.TestCase):
         self.myapp.hide()
 
     def tearDown(self) -> None:
+        Path('checkcif-1979688-finalcif.html').unlink(missing_ok=True)
         Path('1979688-finalcif.chk').unlink(missing_ok=True)
-        Path('1979688.chk').unlink(missing_ok=True)
         Path('1979688-finalcif.ckf').unlink(missing_ok=True)
-        Path('1979688.ckf').unlink(missing_ok=True)
+        Path('1979688-finalcif.vrf').unlink(missing_ok=True)
         Path('1979688-finalcif.cif').unlink(missing_ok=True)
+        Path('1979688-finalcif.gif').unlink(missing_ok=True)
         Path('1979688-finalcif.fcf').unlink(missing_ok=True)
+        Path('1979688.ckf').unlink(missing_ok=True)
+        Path('1979688.chk').unlink(missing_ok=True)
         Path('check.def').unlink(missing_ok=True)
         Path('platon.out').unlink(missing_ok=True)
         Path('examples/work/platon.out').unlink(missing_ok=True)
@@ -42,3 +45,8 @@ class TestPlaton(unittest.TestCase):
         self.myapp.ui.CheckcifButton.click()
         time.sleep(0.3)
         self.assertEqual('FINALCIF V{}'.format(VERSION) in Path('1979688-finalcif.chk').read_text(), True)
+
+    def test_offline_checkcif_writes_gif(self):
+        self.myapp.hide()
+        self.myapp.ui.CheckcifButton.click()
+        self.assertFalse(Path('1979688-finalcif.gif').exists())
