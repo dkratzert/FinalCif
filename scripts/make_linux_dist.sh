774448380bf077ce489789c9bdda485802133591
Daniel Kratzert <dkratzert@gmx.de>
2020-11-10 08:05:06 +0100

    improved release scripts

--- scripts/make_linux_dist.sh
+++ scripts/make_linux_dist.sh
@@ -8,3 +8,9 @@ pip install pip -U
 pip3 install -r requirements.txt
 
 pyinstaller Finalcif_onefile.spec --clean -y
+
+VER=$(cat tools/version.py |grep VERSION |cut -d ' ' -f 3)
+
+mv dist/Finalcif dist/FinalCif-v"$VER"_opensuse
+
+echo "FinalCif version ""$VER"" finished"
--- scripts/make_mac_dist.sh
+++ scripts/make_mac_dist.sh
@@ -8,3 +8,13 @@ pip install pip -U
 pip3 install -r requirements.txt
 
 pyinstaller Finalcif_mac.spec --clean -y
+
+VER=$(cat tools/version.py |grep VERSION |cut -d ' ' -f 3)
+
+mv dist/Finalcif-v_macos.app dist/Finalcif-v"$VER"_macos.app
+
+cd dist || exit
+
+zip -r Finalcif-v"$VER"_macos.app.zip Finalcif-v"$VER"_macos.app/
+
+echo "FinalCif version ""$VER"" finished"
