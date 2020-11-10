42b37842cebad356541d902f54ce8b5b85424d58
Daniel Kratzert <dkratzert@gmx.de>
2020-11-10 08:06:24 +0100

    Update make_mac_dist.sh

--- scripts/make_mac_dist.sh
+++ scripts/make_mac_dist.sh
@@ -14,7 +14,8 @@ VER=$(cat tools/version.py |grep VERSION |cut -d ' ' -f 3)
 mv dist/Finalcif-v_macos.app dist/Finalcif-v"$VER"_macos.app
 
 cd dist || exit
+rm finalcif
 
-zip -r Finalcif-v"$VER"_macos.app.zip Finalcif-v"$VER"_macos.app/
+zip -rm Finalcif-v"$VER"_macos.app.zip Finalcif-v"$VER"_macos.app/
 
 echo "FinalCif version ""$VER"" finished"
