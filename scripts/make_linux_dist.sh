f50858f8017fc0c204b3d1074bc0519ad66fede8
Daniel Kratzert <dkratzert@gmx.de>
2020-11-11 16:13:17 +0100

    Update make_linux_dist.sh

--- scripts/make_linux_dist.sh
+++ scripts/make_linux_dist.sh
@@ -3,6 +3,14 @@ git pull
 
 source venv/bin/activate
 
+DISTR="linux"
+if cat /etc/os-release |grep -c "ubuntu" -gt 0; then
+  DISTR="ubuntu"
+fi
+if cat /etc/os-release |grep -c "suse" -gt 0; then
+  DISTR="opensuse"
+fi
+
 pip install pip -U
 
 pip3 install -r requirements.txt
@@ -11,6 +19,6 @@ pyinstaller Finalcif_onefile.spec --clean -y
 
 VER=$(cat tools/version.py | grep VERSION | cut -d ' ' -f 3)
 
-mv dist/FinalCif "dist/FinalCif-v${VER}_opensuse"
+mv dist/FinalCif "dist/FinalCif-v${VER}_${DISTR}"
 
 echo "FinalCif version ${VER} finished"
