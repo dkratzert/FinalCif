2788c278c979a412f86f3e410c0d771463a20f7f
Daniel Kratzert <dkratzert@gmx.de>
2020-11-11 16:22:20 +0100

    Update make_linux_dist.sh

--- scripts/make_linux_dist.sh
+++ scripts/make_linux_dist.sh
@@ -4,12 +4,15 @@ git pull
 source venv/bin/activate
 
 DISTR="linux"
-if cat /etc/os-release |grep -c "ubuntu" -gt 0; then
+if [[ $(cat /etc/os-release |grep -c "ubuntu") -gt 0 ]]
+then
   DISTR="ubuntu"
 fi
-if cat /etc/os-release |grep -c "suse" -gt 0; then
+if [[ $(cat /etc/os-release |grep -c "suse") -gt 0 ]]
+then
   DISTR="opensuse"
 fi
+
 
 pip install pip -U
 
