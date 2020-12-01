5946c43706011b83d44f1c2fef80fb20e3ef4160
Daniel Kratzert <dkratzert@gmx.de>
2020-12-01 14:42:25 +0100

    Update make_win_release.bat

--- scripts/make_win_release.bat
+++ scripts/make_win_release.bat
@@ -1,10 +1,10 @@
 
 REM execute me from the main directory
 
-CALL venv\Scripts\activate.bat
+REM CALL venv\Scripts\activate.bat
 
-pip install pip -U
+venv\Scripts\pip.exe install pip -U
 
-pip install -r requirements.txt
+venv\Scripts\pip.exe install -r requirements.txt
 
 venv\Scripts\python.exe scripts\make_win_release.py
