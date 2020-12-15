8e1e68e2516e6106923cf16379edf6de0474b119
Daniel Kratzert <dkratzert@gmx.de>
2020-12-15 12:01:00 +0100

    avoid problems with pip upgrade in windows

--- scripts/make_win_release.bat
+++ scripts/make_win_release.bat
@@ -3,8 +3,8 @@ REM execute me from the main directory
 
 CALL venv\Scripts\activate.bat
 
-venv\Scripts\pip.exe install pip -U
+venv\Scripts\python -m pip install -U pip
 
-venv\Scripts\pip.exe install -r requirements.txt
+venv\Scripts\pip3.exe install -r requirements.txt
 
 venv\Scripts\python.exe scripts\make_win_release.py
