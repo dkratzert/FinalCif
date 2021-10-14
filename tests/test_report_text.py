806cb325d8b873aed3919dacc7a5e7ade28e3c2f
Daniel Kratzert <dkratzert@gmx.de>
2021-10-14 13:39:56 +0200

    small fix of the report text

--- tests/test_report_text.py
+++ tests/test_report_text.py
@@ -26,12 +26,16 @@ class TestMachineType(TestCase):
 
     def test__get_cooling_device_iucr(self):
         cif = CifContainer('tests/statics/temp_device_iucr.cif')
-        self.assertEqual('Oxford Cryostream 850', MachineType._get_cooling_device(cif))
+        self.assertEqual('Oxford Cryostream 850 ', MachineType._get_cooling_device(cif))
 
     def test__get_cooling_device_olx(self):
         cif = CifContainer('tests/statics/temp_device_olx.cif')
-        self.assertEqual('Oxford Cryostream 810', MachineType._get_cooling_device(cif))
+        self.assertEqual('Oxford Cryostream 810 ', MachineType._get_cooling_device(cif))
 
     def test__get_cooling_device_both(self):
         cif = CifContainer('tests/statics/temp_device_both.cif')
-        self.assertEqual('Oxford Cryostream 900', MachineType._get_cooling_device(cif))
+        self.assertEqual('Oxford Cryostream 900 ', MachineType._get_cooling_device(cif))
+
+    def test__get_no_cooling_device(self):
+        cif = CifContainer('tests/statics/temp_no_device.cif')
+        self.assertEqual('', MachineType._get_cooling_device(cif))
