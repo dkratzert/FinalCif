3df7944c58be85e03d5b0880d0409722235dcaf9
Daniel Kratzert <dkratzert@gmx.de>
2022-02-05 06:06:10 +0100

    test for nosphera2 cif

---
+++ tests/test_nosperat2_cif.py
@@ -0,0 +1,42 @@
+import unittest
+
+from finalcif.cif.cif_file_io import CifContainer
+from tests.test_utils import current_file_path
+
+
+class TestNosphera2(unittest.TestCase):
+    """
+    CIF files from Nosphera2 have no _atom_site_disorder_group. This test checks if the CIF is
+    nevertheless readable.
+    """
+    def setUp(self) -> None:
+        current_file_path()
+        self.cif = CifContainer('test-data/nospera2.cif')
+
+    def test_atoms(self):
+        self.assertEqual("Atom(label='O1', type='O', x='0.282284(16)', y='0.56940(2)', "
+                         "z='0.869432(5)', part='0', occ='1.000000', u_eq='0.02037(4)')",
+                         str(list(self.cif.atoms())[0]))
+
+    def test_natoms(self):
+        self.assertEqual(9, self.cif.natoms())
+
+    def test_natoms3(self):
+        self.assertEqual("['O1', 'O', 0.282284, 0.5694, 0.869432, 0, 1.0, 0.02037]",
+                         str(list(self.cif.atoms_fract)[0]))
+
+    def test_atomic_struct_site(self):
+        atom = self.cif.atomic_struct.sites
+        self.assertEqual('<gemmi.SmallStructure.Site O1>', str(atom[0]))
+
+    def test_atomic_struct_site_occ(self):
+        atom = self.cif.atomic_struct.sites[0]
+        self.assertEqual(1.0, atom.occ)
+
+    def test_atomic_struct_site_disorder_group(self):
+        atom = self.cif.atomic_struct.sites[0]
+        self.assertEqual(0, atom.disorder_group)
+
+    def test_atomic_struct_site_uiso(self):
+        atom = self.cif.atomic_struct.sites[0]
+        self.assertEqual(0.02037, atom.u_iso)
