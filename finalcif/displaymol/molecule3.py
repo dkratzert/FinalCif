bca0eb54253623199acfaa9c325e7237ae190afb
Daniel Kratzert <dkratzert@gmx.de>
2022-02-21 22:50:54 +0100

    correctly render atoms

--- finalcif/displaymol/molecule3.py
+++ finalcif/displaymol/molecule3.py
@@ -5,17 +5,21 @@ ellipsoids (vtkTensorGlyphs) plotted against the average structure (in tube
 representation."""
 import sys
 
-import numpy
 import vtk
 from PyQt5 import QtWidgets, QtCore
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
+from finalcif.cif.atoms import element2num, get_radius_from_element
 from finalcif.cif.cif_file_io import CifContainer
+from finalcif.tools.misc import distance
 
 
 class MoleculeWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent, cif: CifContainer):
+        super().__init__(parent=parent)
+
+        self.cif = cif
+
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.vlayout = QtWidgets.QVBoxLayout(self)
         self.vlayout.setContentsMargins(0, 0, 0, 0)
@@ -25,13 +29,29 @@ class MoleculeWidget(QtWidgets.QWidget):
         istyle = vtk.vtkInteractorStyleSwitch()
         istyle.SetCurrentStyleToTrackballCamera()
 
-
         self.vtkWidget = QVTKRenderWindowInteractor(self)
         self.vlayout.addWidget(self.vtkWidget)
         interactor = self.vtkWidget.GetRenderWindow().GetInteractor()
         interactor.SetInteractorStyle(istyle)
 
-        molecule = self.get_molecule()
+        # molecule = self.get_molecule()
+        molecule = self.add_atoms()
+
+        """ Does not work, why?
+        colors = vtk.vtkDoubleArray()
+        colors.SetNumberOfComponents(3)
+        colors.SetName("Colors")
+        colors.Allocate(3 * molecule.GetNumberOfAtoms()+1)
+        for i in range(molecule.GetNumberOfAtoms()):
+            num = molecule.GetAtomAtomicNumber(i)
+            #print(i, element2rgb[num2element[num]])
+            colors.InsertNextTypedTuple(element2rgb[num2element[num]])
+            #colors.InsertNextTypedTuple((1, 1, 1))
+            #print(element2rgb[num2element[num]])
+        molecule.GetAtomData().AddArray(colors)
+        # molMapper.SetInputArrayToProcess(0, 0, 0, vtkDataObject.FIELD_ASSOCIATION_VERTICES, "Colors")
+        # molMapper.SetInputArrayToProcess(0, 0, 0, 4, "Colors")
+        """
 
         molMapper = vtk.vtkMoleculeMapper()
         molMapper.SetInputData(molecule)
@@ -48,7 +68,6 @@ class MoleculeWidget(QtWidgets.QWidget):
 
         renderer = vtk.vtkRenderer()
         renderer.AddActor(molActor)
-        # renderer.AddActor(ellActor)
         renderer.SetBackground(255, 255, 255)
         renderer.SetLayer(0)
         renderer.ResetCamera()
@@ -56,52 +75,30 @@ class MoleculeWidget(QtWidgets.QWidget):
         self.vtkWidget.GetRenderWindow().AddRenderer(renderer)
         interactor.Initialize()
 
-    def get_molecule(self):
-        symdict = {"N": 7, "C": 6, "O": 8, "H": 1}
-        coords = []
-        symbols = []
-        pdbf = open("finalcif/displaymol/2evq.pdb")
-        line = pdbf.readline()
-        natoms = 0
-        while line[:6] != "MODEL ":
-            line = pdbf.readline()
-        while line:
-            modelno = int(line[6:])
-            line = pdbf.readline()
-            i = 0
-            while line[:6] != "ENDMDL":
-                sym = line[12:16].strip()
-                if sym[:1] in ["N", "C", "O"]:
-                    if modelno == 1:
-                        coords.append([[], [], []])
-                        natoms += 1
-                        symbols.append(symdict[sym[0]])
-                    x = float(line[30:38])
-                    y = float(line[38:46])
-                    z = float(line[46:54])
-                    coords[i][0].append(x)
-                    coords[i][1].append(y)
-                    coords[i][2].append(z)
-                    i += 1
-                line = pdbf.readline()
-            while line and line[:6] != "MODEL ":
-                line = pdbf.readline()
-        pdbf.close()
-        coords = numpy.array(coords)
-        averaged = numpy.mean(coords, axis=2)
+    def add_atoms(self):
         molecule = vtk.vtkMolecule()
-        for i in range(natoms):
-            s = symbols[i]
-            a = averaged[i]
-            molecule.AppendAtom(s, a[0], a[1], a[2])
-        for i in range(natoms):
-            a = averaged[i]
-            for j in range(i + 1, natoms):
-                b = averaged[j]
-                d = numpy.sqrt(numpy.sum((a - b) ** 2))
-                if d < 1.7:
-                    molecule.AppendBond(i, j, 1)
+        for atom in self.cif.atoms_orth:
+            vatom = molecule.AppendAtom(element2num[atom.type], atom.x, atom.y, atom.z)
+        self.make_bonds(molecule)
         return molecule
+
+    def make_bonds(self, molecule, extra_param: float = 0.48) -> None:
+        h_atoms = ('H', 'D')
+        for num1, at1 in enumerate(self.cif.atoms_orth, 0):
+            rad1 = get_radius_from_element(at1.type)
+            for num2, at2 in enumerate(self.cif.atoms_orth, 0):
+                if at1.part * at2.part != 0 and at1.part != at2.part:
+                    continue
+                if at1.label == at2.label:
+                    continue
+                d = distance(at1.x, at1.y, at1.z, at2.x, at2.y, at2.z)
+                if d > 4.0:  # makes bonding faster (longer bonds do not exist)
+                    continue
+                rad2 = get_radius_from_element(at2.type)
+                if (rad1 + rad2) + extra_param > d > 0:
+                    if at1.type in h_atoms and at2.type in h_atoms:
+                        continue
+                    molecule.AppendBond(num1, num2, 1)
 
 
 if __name__ == "__main__":
@@ -114,7 +111,7 @@ if __name__ == "__main__":
     # shx.read_file('tests/examples/1979688-finalcif.res')
     # atoms = [x.cart_coords for x in shx.atoms]
     cif = CifContainer('tests/examples/1979688.cif')
-    render_widget = MoleculeWidget()  # cif.atoms_orth)
+    render_widget = MoleculeWidget(None, cif)  # cif.atoms_orth)
     # add and show
     window.setCentralWidget(render_widget)
     window.setMinimumSize(500, 500)
