bc9a6bb0444f35a86374dff53d5a921d61a03941
Daniel Kratzert <dkratzert@gmx.de>
2022-02-23 07:59:14 +0100

    old stuff out

--- finalcif/appwindow.py
+++ finalcif/appwindow.py
@@ -1441,21 +1441,7 @@ class AppWindow(QMainWindow):
         peak = self.cif['_refine_diff_density_max']
         if peak:
             self.ui.peakLineEdit.setText("{} / {}".format(peak, self.cif['_refine_diff_density_min']))
-        if 'darwin' in sys.platform and getattr(sys, 'frozen', False):
-            self.ui.moleculeLayout.addWidget(QLabel('                     '
-                                                    'FinalCif is currently unable to draw molecules in Mac OS'))
-            # Currently, the QWebEngineView doesn't work on macOS due to pyinstaller issues.
-            return None
-        elif 'linux' in sys.platform:
-            self.ui.moleculeLayout.addWidget(QLabel('                     '
-                                                    'FinalCif is currently unable to draw molecules in Linux.'))
-            # The license of JSmol-Lite is unclear, and I need to make an own molecule viewer anyway.
-            return None
-        try:
-            self.init_webview()
-        except Exception as e:
-            print(e, '###')
-        #self.view_molecule()
+        self.view_molecule()
 
     def view_molecule(self) -> None:
         if self.ui.growCheckBox.isChecked():
@@ -1472,7 +1458,7 @@ class AppWindow(QMainWindow):
                     atoms = []
         else:
             self.ui.molGroupBox.setTitle('Asymmetric Unit')
-            atoms = self.cif.atoms_orth
+            #atoms = self.cif.atoms_orth
         render_widget = MoleculeWidget(None, cif)
 
 
