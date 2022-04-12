a4edb16e07a2e7661bf227e4d02cc961ba283c1d
Daniel Kratzert <dkratzert@gmx.de>
2022-04-12 15:50:28 +0200

    Fix tests in windows

--- tests/test_nosperat2_cif.py
+++ tests/test_nosperat2_cif.py
@@ -43,10 +43,11 @@ class TestNosphera2(unittest.TestCase):
     def test_shelx_file(self):
         # Even though, this gives an empty string: -->
         self.assertEqual('', self.cif['_shelx_res_file'])
-        self.assertEqual('TITL mo_1558 in P2(1)/n', self.cif['_iucr_refine_instructions_details'][:24].lstrip())
+        self.assertEqual('TITL mo_1558 in P2(1)/n',
+                         self.cif['_iucr_refine_instructions_details'].lstrip('\r\n')[:24].strip('\r\n'))
 
     def test_iucr_refine_instructions(self):
         # <-- We have res file data, because there is iucr_refine_instructions:
         self.assertEqual(''';
 TITL mo_1558 in P2(1)/n
-CELL 0.71073 10.3975 7.4''', self.cif.res_file_data[:50])
+CELL 0.71073 10.3975 7.4''', self.cif.res_file_data.lstrip()[:50].strip('\r\n'))
