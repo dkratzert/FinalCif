3bfa1b92270e6417b97a695751bf18e9e7f0ecc9
Daniel Kratzert <dkratzert@gmx.de>
2022-07-11 22:19:02 +0200

    Fixes initialization order of ShelxFile in CifContainer

--- tests/test_report_text.py
+++ tests/test_report_text.py
@@ -6,22 +6,6 @@ from finalcif.cif.cif_file_io import CifContainer
 from finalcif.report.report_text import Hydrogens, MachineType
 
 
-class TestHydrogens(TestCase):
-    def setUp(self) -> None:
-        document = Document()
-        self.paragraph = document.add_paragraph()
-
-    def test_has_no_isotropic_displacement_parameters(self):
-        cif = CifContainer('test-data/p21c.cif')
-        h = Hydrogens(cif, self.paragraph)
-        self.assertEqual(0, h.number_of_isotropic_atoms())
-
-    def test_has_isotropic_displacement_parameters(self):
-        cif = CifContainer('test-data/1923_Aminoff, G._Ni As_P 63.m m c_Nickel arsenide.cif')
-        h = Hydrogens(cif, self.paragraph)
-        self.assertEqual(2, h.number_of_isotropic_atoms())
-
-
 class TestMachineType(TestCase):
 
     def test__get_cooling_device_iucr(self):
