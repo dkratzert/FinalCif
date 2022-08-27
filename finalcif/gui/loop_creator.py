d82e1ca72e632ae484efca2772d8b0c7eb8d3579
Daniel Kratzert <dkratzert@gmx.de>
2022-08-27 19:44:09 +0200

    refactoring

--- finalcif/gui/loop_creator.py
+++ finalcif/gui/loop_creator.py
@@ -4,12 +4,14 @@ from typing import List
 from PyQt5.QtWidgets import QWidget
 
 from finalcif.cif import all_cif_dicts
+from finalcif.cif.cif_file_io import CifContainer
 from finalcif.gui.loop_creator_ui import Ui_LoopCreator
 
 
 class LoopCreator(QWidget, Ui_LoopCreator):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None, cif: CifContainer = None):
+        super().__init__(parent=parent)
+        self.cif = cif
         self.setupUi(self)
         self.leftPushButton.clicked.connect(self.move_selected_key_to_left)
         self.rightPushButton.clicked.connect(self.move_selected_key_to_right)
@@ -23,6 +25,12 @@ class LoopCreator(QWidget, Ui_LoopCreator):
     @property
     def tags(self):
         return self.get_itemtexts_from_new_loop()
+
+    def save_new_loop_to_cif(self):
+        loop = self.cif.add_loop_to_cif(loop_tags=self.tags, loop_values=('?',) * len(self.tags))
+        if not self.parent():
+            self.cif.save()
+        return loop
 
     def search(self, searchtext: str):
         self.availableKeysListWidget.clear()
@@ -59,3 +67,15 @@ class LoopCreator(QWidget, Ui_LoopCreator):
                 if item.text() == current_item.text():
                     item.setHidden(False)
             current_item.setHidden(True)
+
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    cif = CifContainer('test-data/1000006.cif')
+    lc = LoopCreator(cif=cif)
+    lc.saveLoopPushButton.clicked.connect(lc.save_new_loop_to_cif)
+    lc.show()
+    sys.exit(app.exec_())
