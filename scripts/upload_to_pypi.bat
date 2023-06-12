9286e9a05eff66fbe3028aecd41f0ca1577d5ca1
Daniel Kratzert <dkratzert@gmx.de>
2023-06-12 20:32:58 +0200

    Check twine before upload [no ci]

--- scripts/upload_to_pypi.bat
+++ scripts/upload_to_pypi.bat
@@ -1,6 +1,8 @@
 call venv\Scripts\activate.bat
 venv\Scripts\pip install twine -U
 
+venv\Scripts\python -m twine check dist\*
+
 venv\Scripts\python -m twine upload --repository pypi dist\*
 
 echo "Now add a Git tag!"
