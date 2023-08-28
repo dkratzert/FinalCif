1ae7107f94cd7872f8522f73071161a90a763c33
Daniel Kratzert <dkratzert@gmx.de>
2023-08-28 10:52:13 +0200

    Re-add splitter and remove fixed width of sidebar

--- finalcif/appwindow.py
+++ finalcif/appwindow.py
@@ -610,11 +610,11 @@ class AppWindow(QMainWindow):
         super(AppWindow, self).resizeEvent(a0)
         with suppress(AttributeError):
             self._savesize()
-        main_width = self.ui.Mainwidget.width()
-        left_frame = main_width * 0.22
-        if left_frame <= 300:
-            left_frame = 300
-        self.ui.LeftFrame.setFixedWidth(int(left_frame))
+        #main_width = self.ui.Mainwidget.width()
+        #left_frame = main_width * 0.22
+        #if left_frame <= 300:
+        #    left_frame = 300
+        #self.ui.LeftFrame.setFixedWidth(int(left_frame))
         self.ui.cif_main_table.resizeRowsToContents()
 
     def moveEvent(self, a0: QtGui.QMoveEvent) -> None:
