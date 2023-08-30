bb1d346c870edc92dfd0516a2478beee319f2856
Daniel Kratzert <dkratzert@gmx.de>
2023-08-30 22:21:02 +0200

    Adds icon recource

--- scripts/starter/finalcif.cpp
+++ scripts/starter/finalcif.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <cstdlib>
+/*
+For the windows icon:
+windres -i icon.rc -o resource.o
+g++ -o finalcif.exe finalcif.cpp resource.o
+*/
+
+#ifdef _WIN32
+    #include <windows.h>
+    #include "resource.h"
+#endif
 
 int main() {
     std::cout << "Starting FinalCif..." << std::endl;
-    setenv("PYTHONPATH", ".", 1);
+    #ifdef _WIN32
+        SetEnvironmentVariableA("PYTHONPATH", ".");
+    #else
+        setenv("PYTHONPATH", ".", 1);
+    #endif
     std::system("python3 finalcif/finalcif_start.py");
     return 0;
 }
