b0cf6681e2b693060dd788b752ad247f0f0127f3
Daniel Kratzert <dkratzert@gmx.de>
2023-08-30 22:09:43 +0200

    Use really simple python loading for c++

--- scripts/starter/finalcif.cpp
+++ scripts/starter/finalcif.cpp
@@ -1,36 +1,11 @@
-#include <stdio.h>
-#include <conio.h>
-#include <Python.h>
-
-int main()
-{
-	char filename[] = ("finalcif/finalcif_start.py");
-
-	FILE* fp;
-
-	Py_Initialize();
-
-	//fp = _Py_fopen(filename, "r");
-	//PyRun_SimpleFile(fp, filename);
-	PyRun_SimpleString("from time import time,ctime\n"
-		"print('Today is', ctime(time()))\n");
-
-	Py_Finalize();
-	return 0;
-}
-
-// or just
-
-/*
-
 #include <iostream>
 #include <cstdlib>
 
 int main() {
-    std::cout << "Starte Python-Applikation..." << std::endl;
-    std::system("python3 pfad_zu_deiner_python_datei.py");
+    std::cout << "Starting FinalCif..." << std::endl;
+    setenv("PYTHONPATH", ".", 1);
+    std::system("python3 finalcif/finalcif_start.py");
     return 0;
 }
 
 
-*/
