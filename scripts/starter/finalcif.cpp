b35725c252acc7ab2fd7c3fb95f22f389a710be5
Daniel Kratzert <dkratzert@gmx.de>
2023-08-31 09:38:50 +0200

    finalcif.exe works now

--- scripts/starter/finalcif.cpp
+++ scripts/starter/finalcif.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 #include <cstdlib>
-/*
-For the windows icon:
-windres -i icon.rc -o resource.o
-g++ -o finalcif.exe finalcif.cpp resource.o
-*/
 
 #ifdef _WIN32
     #include <windows.h>
-    #include "resource.h"
 #endif
+
 
 int main() {
     std::cout << "Starting FinalCif..." << std::endl;
-    #ifdef _WIN32
-        SetEnvironmentVariableA("PYTHONPATH", ".");
-    #else
-        setenv("PYTHONPATH", ".", 1);
-    #endif
-    std::system("python3 finalcif/finalcif_start.py");
+#ifdef _WIN32
+    _putenv_s("PYTHONPATH", ".");
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow(hWnd, SW_HIDE);  // Hide the console window
+#else
+    setenv("PYTHONPATH", ".", 1);
+#endif
+    std::system("venv\\Scripts\\pythonw finalcif/finalcif_start.py");
     return 0;
 }
+
 
 
