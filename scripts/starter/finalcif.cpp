1e993e7da6fc4b5e72a30e6e485b146a0084aa4b
Daniel Kratzert <dkratzert@gmx.de>
2023-09-05 09:27:31 +0200

    Makes executable work with ShelXle

--- scripts/starter/finalcif.cpp
+++ scripts/starter/finalcif.cpp
@@ -1,41 +1,10 @@
 #include <iostream>
 #include <cstdlib>
-using namespace std;
-
-#ifdef _WIN32
 #include <windows.h>
 #include "finalcif.h"
-#endif
 
+using namespace std;
 
-int main(int argc, char* argv[]) {
-	
-	cout << "Starting FinalCif..." << endl;
-
-#ifdef _WIN32
-	_putenv_s("PYTHONPATH", ".");
-
-	bool hide = true;
-	for (int i = 1; i < argc; ++i) {
-		if (strcmp(argv[i], "-nohide") == 0) {
-			hide = false;
-			argv[i] = "";
-			break;
-		}
-	}
-	if (hide) {
-		HWND hWnd = GetConsoleWindow();
-		ShowWindow(hWnd, SW_HIDE);  // Hide the console window
-	}
-#else
-	setenv("PYTHONPATH", ".", 1);
-#endif
-	string command = "python finalcif/finalcif_start.py";
-	add_argvalues(command, argc, argv);
-	// cout << command << endl;
-	system(command.c_str());
-	return 0;
-}
 
 void add_argvalues(string& command, int argc, char* argv[])
 {
@@ -43,6 +12,64 @@ void add_argvalues(string& command, int argc, char* argv[])
 		command += " ";
 		command += argv[i];
 	}
+}
+
+
+std::string getParentDirectory(const std::string& filePath) {
+	size_t found = filePath.find_last_of("/\\");
+	if (found != std::string::npos) {
+		return filePath.substr(0, found);
+	}
+	// No parent directory found or empty string if already in root
+	return "";
+}
+
+
+void hide_console_window()
+{
+	HWND hWnd = GetConsoleWindow();
+	ShowWindow(hWnd, SW_HIDE);
+}
+
+void should_window_hide(int argc, char* argv[], bool& hide)
+{
+	for (int i = 1; i < argc; ++i) {
+		if (strcmp(argv[i], "-nohide") == 0) {
+			hide = false;
+			argv[i] = "";
+			break;
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+
+	cout << "Starting FinalCif..." << endl;
+
+	char current_dir[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, current_dir);
+
+	char current_executable_filepath[MAX_PATH];
+	DWORD len = GetModuleFileName(NULL, current_executable_filepath, MAX_PATH);
+	string parent_dir = getParentDirectory(current_executable_filepath);
+	// Not good, because ShelXle wants to start it from the res file directory:
+	// SetCurrentDirectory(parent_dir.c_str());
+
+	bool hide = true;
+	should_window_hide(argc, argv, hide);
+	if (hide) {
+		hide_console_window();
+	}
+
+	string command = "\"" + parent_dir + "\\python.exe" + "\" \"" + parent_dir + "\\finalcif\\finalcif_start.py\"";
+	add_argvalues(command, argc, argv);
+	if (!hide) {
+		cout << "Running command: '" << command.c_str() << "' Inside of: " << std::string(current_dir) << endl;
+	}
+
+	WinExec(command.c_str(), SW_HIDE);
+	
+	return 0;
 }
 
 
