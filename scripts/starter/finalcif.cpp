88bed38d4934ff9aeb741a3f23d9ed7642888f16
Daniel Kratzert <dkratzert@gmx.de>
2023-10-12 17:13:35 +0200

    Improved startup executable
    * Still missing proper argument handling

--- scripts/starter/finalcif.cpp
+++ scripts/starter/finalcif.cpp
@@ -1,7 +1,11 @@
+#include <fstream>
+#include <sstream>
 #include <iostream>
 #include <cstdlib>
 #include <windows.h>
 #include "finalcif.h"
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
 using namespace std;
 
@@ -42,20 +46,8 @@ void should_window_hide(int argc, char* argv[], bool& hide)
 	}
 }
 
-int main(int argc, char* argv[]) {
-
-	cout << "Starting FinalCif..." << endl;
-
-	// This dir may not be the path of the Python installation!
-	char current_dir[MAX_PATH];
-	GetCurrentDirectory(MAX_PATH, current_dir);
-
-	char current_executable_filepath[MAX_PATH];
-	DWORD len = GetModuleFileName(NULL, current_executable_filepath, MAX_PATH);
-	string parent_dir = getParentDirectory(current_executable_filepath);
-	// Not good, because ShelXle wants to start it from the res file directory:
-	// SetCurrentDirectory(parent_dir.c_str());
-
+void run_from_commandline(int argc, char* argv[], std::string& parent_dir, char  current_dir[260])
+{
 	bool hide = true;
 	should_window_hide(argc, argv, hide);
 	if (hide) {
@@ -69,6 +61,54 @@ int main(int argc, char* argv[]) {
 	}
 
 	WinExec(command.c_str(), SW_SHOW);
+}
+
+std::string readFileIntoString(const std::string& path) {
+	std::ifstream fileStream(path);
+	cout << path << endl;;
+	if (!fileStream) {
+		std::cerr << "Datei konnte nicht geöffnet werden: " << path << std::endl;
+		return "";
+	}
+
+	std::stringstream stringStream;
+	stringStream << fileStream.rdbuf();
+
+	return stringStream.str();
+}
+
+int main(int argc, char* argv[]) {
+
+	//cout << "Starting FinalCif..." << endl;
+
+	// This dir may not be the path of the Python installation!
+	char current_dir[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, current_dir);
+
+	char current_executable_filepath[MAX_PATH];
+	DWORD len = GetModuleFileName(NULL, current_executable_filepath, MAX_PATH);
+	string parent_dir = getParentDirectory(current_executable_filepath);
+	// Not good, because ShelXle wants to start it from the res file directory:
+	// SetCurrentDirectory(parent_dir.c_str());
+
+	//run_from_commandline(argc, argv, parent_dir, current_dir);
+
+	wchar_t* program = Py_DecodeLocale(argv[0], NULL);
+	if (program == NULL) {
+		fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+		exit(1);
+	}
+	string runscript = readFileIntoString(parent_dir + "/finalcif/finalcif_start.py");
+	Py_Initialize();
+	if (argc > 1) {
+		string arg_command = string("import sys\nsys.argv.append(r'") + argv[1] + "')";
+		PyRun_SimpleString(arg_command.c_str());
+	}
+	PyRun_SimpleString(runscript.c_str());
+	if (Py_FinalizeEx() < 0) {
+		exit(-1);
+	}
+	PyMem_RawFree(program);
 	
 	return 0;
 }
--- scripts/starter/finalcif.h
+++ scripts/starter/finalcif.h
@@ -5,3 +5,7 @@ void add_argvalues(std::string& command, int argc, char* argv[]);
 void hide_console_window();
 
 void should_window_hide(int argc, char* argv[], bool& hide);
+
+void run_from_commandline(int argc, char* argv[], std::string& parent_dir, char  current_dir[260]);
+
+int main(int argc, char* argv[]);
