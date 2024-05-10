6935f2fef1e7f3a91c0eedabfe62b45949b710d3
Daniel Kratzert <dkratzert@gmx.de>
2024-05-10 20:26:25 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 from finalcif import VERSION
 
 project = 'FinalCif'
-copyright = '2022, Daniel Kratzert'
+copyright = '2024, Daniel Kratzert'
 author = 'Daniel Kratzert'
 
 # The full version, including alpha/beta/rc tags
