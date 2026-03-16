ce3412bdfa05f595a00139b88586fed4dd1d3832
Daniel Kratzert <dkratzert@gmx.de>
2026-03-16 17:21:12 +0100

    Update finalcif.cpp

--- scripts/starter/finalcif.cpp
+++ scripts/starter/finalcif.cpp
@@ -1,84 +1,72 @@
 #define PY_SSIZE_T_CLEAN
-#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
-
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <cstdlib>
-#include <locale>
-#include <format>
-#include <codecvt>
-#include <windows.h>
-#include "finalcif.h"
 #include <Python.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
 
 
-using namespace std;
-
-
-std::string getParentDirectory(const std::string& filePath) {
-    size_t found = filePath.find_last_of("/\\");
-    if (found != std::string::npos) {
-        return filePath.substr(0, found);
-    }
-    // No parent directory found or empty string if already in root
-    return "";
-}
-
-
-std::string to_utf8(const std::wstring& wstr) {
-    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
-    return converter.to_bytes(wstr);
-}
-
-int wmain(int argc, wchar_t* argv[]) {
-
-    cout << "Starting FinalCif..." << endl;
-
-    // This dir may not be the path of the Python installation!
-    char current_dir[MAX_PATH];
-    GetCurrentDirectory(MAX_PATH, current_dir);
-
-    char current_executable_filepath[MAX_PATH];
-    DWORD len = GetModuleFileName(NULL, current_executable_filepath, MAX_PATH);
-    string parent_dir = getParentDirectory(current_executable_filepath);
-    // Not good, because ShelXle wants to start it from the res file directory:
-    // SetCurrentDirectory(parent_dir.c_str());
-
-    wchar_t* program = Py_DecodeLocale(to_utf8(argv[0]).c_str(), NULL);
-    if (program == NULL) {
-        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
-        exit(1);
-    }
-
-    string runscript_path = parent_dir + "/finalcif/finalcif_start.py";
-
-    FILE* fp = fopen(runscript_path.c_str(), "rb");
-    if (fp == NULL)
+void attach_console()
+{
+    if (AttachConsole(ATTACH_PARENT_PROCESS))
     {
-        cout << "Error opening file: " << runscript_path << endl;
-        exit(1);
+        FILE* dummy;
+        freopen_s(&dummy, "CONOUT$", "w", stdout);
+        freopen_s(&dummy, "CONOUT$", "w", stderr);
+        freopen_s(&dummy, "CONIN$", "r", stdin);
     }
+}
+
+
+std::string get_parent_dir(const std::string& path)
+{
+    size_t pos = path.find_last_of("\\/");
+    if (pos == std::string::npos)
+        return "";
+    return path.substr(0, pos);
+}
+
+int wmain(int argc, wchar_t* argv[])
+{
+    attach_console();
+    std::cout << "Starting FinalCif...\n";
+
+    wchar_t exe_path[MAX_PATH];
+    GetModuleFileNameW(NULL, exe_path, MAX_PATH);
+
+    std::wstring exe_w(exe_path);
+    std::string exe_utf8(Py_EncodeLocale(exe_w.c_str(), NULL));
+
+    std::string parent = get_parent_dir(exe_utf8);
+    std::string script = parent + "\\finalcif\\finalcif_start.py";
 
     Py_Initialize();
-   
-    // Add the first argument to the arglist first:
+
+    // sys.argv setzen
+    PyObject* sys = PyImport_ImportModule("sys");
+    PyObject* argv_list = PyList_New(0);
+
+    PyList_Append(argv_list, PyUnicode_FromString(script.c_str()));
+
     if (argc > 1) {
-        string arg_command = format("import sys\nsys.argv.append(r'{}')", to_utf8(argv[1]));
-        PyRun_SimpleString(arg_command.c_str());
+        char* arg = Py_EncodeLocale(argv[1], NULL);
+        PyList_Append(argv_list, PyUnicode_FromString(arg));
+        PyMem_RawFree(arg);
     }
 
-    // Then execute the python script:
-    PyRun_SimpleFileEx(fp, runscript_path.c_str(), true);
+    PyObject_SetAttrString(sys, "argv", argv_list);
+    Py_DECREF(argv_list);
+    Py_DECREF(sys);
 
-    if (Py_FinalizeEx() < 0) {
-        exit(1);
+    FILE* fp = fopen(script.c_str(), "rb");
+    if (!fp) {
+        std::cerr << "Cannot open script: " << script << "\n";
+        return 1;
     }
 
-    PyMem_RawFree(program);
+    PyRun_SimpleFile(fp, script.c_str());
+    fclose(fp);
+
+    Py_Finalize();
 
     return 0;
 }
-
-
-
